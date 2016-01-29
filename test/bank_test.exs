8ee169a61a40e06cea06f21cda941f44aa43ef41
Nikolay Petrachkov <nik@jastix.biz>
2016-01-29 22:52:00 +0100

    [WIP] Start with tests.

--- test/bank_test.exs
+++ test/bank_test.exs
@@ -2,7 +2,14 @@ defmodule BankTest do
   use ExUnit.Case
   doctest Bank
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "#read_csv removes same account transactions" do
+    transactions_csv = "./test/fixtures/ing.csv"
+    records = Bank.read_csv(transactions_csv) |> Enum.to_list
+    assert Enum.count(records) == 6
+  end
+
+  test "#supermarkets counts expenses for supermarkets" do
+    transactions_csv = "./test/fixtures/ing.csv"
+    assert Bank.supermarkets(transactions_csv) == 55.00
   end
 end
